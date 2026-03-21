2593d430bcc4d3ba94fded4b03304b72c7f54f89
mrawa <1251746555@qq.com>
2026-03-21 21:33:45 +0800

    修改Kafka Receiver，把各个渠道的线程池都改为虚拟线程，并在Receiver中设置基于高/低水位的背压机制

--- austin-handler/src/main/java/com/java3y/austin/handler/receiver/service/impl/ConsumeServiceImpl.java
+++ austin-handler/src/main/java/com/java3y/austin/handler/receiver/service/impl/ConsumeServiceImpl.java
@@ -7,11 +7,14 @@ import com.java3y.austin.common.domain.RecallTaskInfo;
 import com.java3y.austin.common.domain.TaskInfo;
 import com.java3y.austin.common.enums.AnchorState;
 import com.java3y.austin.handler.handler.HandlerHolder;
+import com.java3y.austin.handler.handler.VirtualThreadBackPressureManager;
 import com.java3y.austin.handler.pending.Task;
 import com.java3y.austin.handler.pending.TaskPendingHolder;
 import com.java3y.austin.handler.receiver.service.ConsumeService;
 import com.java3y.austin.handler.utils.GroupIdMappingUtils;
 import com.java3y.austin.support.utils.LogUtils;
+import com.java3y.austin.support.utils.ThreadPoolUtils;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
@@ -37,13 +40,26 @@ public class ConsumeServiceImpl implements ConsumeService {
     @Autowired
     private HandlerHolder handlerHolder;
 
+    private VirtualThreadBackPressureManager backPressureManager;
+
+    @PostConstruct
+    public void init() {
+        backPressureManager = new VirtualThreadBackPressureManager(100000, 50000);
+    }
+
     @Override
     public void consume2Send(List<TaskInfo> taskInfoLists) {
-        String topicGroupId = GroupIdMappingUtils.getGroupIdByTaskInfo(CollUtil.getFirst(taskInfoLists.iterator()));
+//        String topicGroupId = GroupIdMappingUtils.getGroupIdByTaskInfo(CollUtil.getFirst(taskInfoLists.iterator()));
         for (TaskInfo taskInfo : taskInfoLists) {
+            backPressureManager.awaitDrainIfNeeded();
             logUtils.print(LogParam.builder().bizType(LOG_BIZ_TYPE).object(taskInfo).build(), AnchorInfo.builder().bizId(taskInfo.getBizId()).messageId(taskInfo.getMessageId()).ids(taskInfo.getReceiver()).businessId(taskInfo.getBusinessId()).state(AnchorState.RECEIVE.getCode()).build());
             Task task = context.getBean(Task.class).setTaskInfo(taskInfo);
-            taskPendingHolder.route(topicGroupId).execute(task);
+//            taskPendingHolder.route(topicGroupId).execute(task);
+            try {
+                ThreadPoolUtils.getVirtualExecutorService().execute(task);
+            } finally {
+                backPressureManager.decrement();
+            }
         }
     }
 
